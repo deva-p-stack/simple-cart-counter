12045cd3fee30a5f44772c8846950804fd0f1e19
deva-p-stack <devap677@gmail.com>
2026-01-20 02:42:29 +0530

    first commit;

--- src/main.js
+++ src/main.js
@@ -1,5 +1,46 @@
 import './style.css'
 
+let counte = 0; 
+
+let addbtn = document.querySelectorAll('.addbtn');
+let counters = document.querySelector('.counter');
+let cart = document.querySelector('.cart');
+let parent = document.querySelector(".parent");
+
+addbtn.forEach((btn)=>{
+
+btn.addEventListener("click",function(){
+
+counte++;
+counters.classList.remove("hidden")   
+counters.textContent = counte;
+
+let newdiv = document.createElement('div');
+newdiv.className = " flex absolute  top-9.5 left-[-45px] px-2 text-xs mx-auto justify-center items-center py-2  w-full min-w-35 rounded-lg bg-gray-300";
+newdiv.textContent = "product add to cart";
+counters.append(newdiv);
+
+   setTimeout(() => {
+      newdiv.remove();
+    }, 1000);
+
+})
+});
+
+cart.addEventListener("click",function(){
+
+    parent.classList.add("hidden");
+    
+
+})
+
+
+
+
+
+
+
+
 let count = 0;
 
 let add = document.querySelector('#add');
@@ -19,9 +60,7 @@ function updatevalue(){
         counter.classList.remove("text-red-500");
         counter.classList.add("text-green-500");
     }
-
 }
-
 
 add.addEventListener("click",function(){
 
@@ -42,3 +81,34 @@ reset.addEventListener("click",function(){
     
      counter.textContent = "0";
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
